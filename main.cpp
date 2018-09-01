d8abb2d3d5d79628890e6b2fe5034cac51b87122
Pepito <juan.agustin.gonzalez99@gmail.com>
2018-09-01 10:49:19 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,88 @@
+//#############################################################################
+// ARCHIVO             : main.cpp
+// AUTOR/ES            : Juan Gonzalez
+// VERSION             : 0.01 beta.
+// FECHA DE CREACION   : 30/08/2018.
+// ULTIMA ACTUALIZACION: dd/mm/aaaa.
+// LICENCIA            : GPL (General Public License) - Version 3.
+//
+//  **************************************************************************
+//  * El software libre no es una cuestion economica sino una cuestion etica *
+//  **************************************************************************
+//
+// Este programa es software libre;  puede redistribuirlo  o  modificarlo bajo
+// los terminos de la Licencia Publica General de GNU  tal como se publica por
+// la  Free Software Foundation;  ya sea la version 3 de la Licencia,  o (a su
+// eleccion) cualquier version posterior.
+//
+// Este programa se distribuye con la esperanza  de que le sea util,  pero SIN
+// NINGUNA  GARANTIA;  sin  incluso  la garantia implicita de MERCANTILIDAD  o
+// IDONEIDAD PARA UN PROPOSITO PARTICULAR.
+//
+// Vea la Licencia Publica General GNU para mas detalles.
+//
+// Deberia haber recibido una copia de la Licencia Publica General de GNU junto
+// con este proyecto, si no es asi, escriba a la Free Software Foundation, Inc,
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307, EE.UU.
+
+//=============================================================================
+// SISTEMA OPERATIVO   : Windows 10.
+// IDE                 : Code::Blocks - 17.12
+// COMPILADOR          : MinGW (Windows).
+// LICENCIA            : GPL (General Public License) - Version 3.
+//=============================================================================
+// DESCRIPCION:
+//              Breve explicacion sobre el contenido del archivo.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+//*****************************************************************************
+//                       CONFIGURACION MULTIPLATAFORMA
+//=============================================================================
+#ifndef _WIN32
+    # define _LINUX
+#endif
+
+//*****************************************************************************
+//                             INCLUSIONES ESTANDAR
+//=============================================================================
+#include <iostream> // Libreria de flujos de  Entrada/Salida  que contiene  los
+                    // objetos cin, cout y endl.
+
+#include <cstdlib>  // Libreria estandar que contiene la funcion exit().
+
+
+//*****************************************************************************
+//                             DECLARACIONES GLOBALES
+//=============================================================================
+
+
+
+//*****************************************************************************
+//                             INCLUSIONES PERSONALES
+//=============================================================================
+#include "CSYSTEM/csystem.h" // Libreria para multiplataforma.
+
+
+//==============================================================================
+// DECLARACION DEL ESPACIO DE NOMBRES POR DEFECTO
+//------------------------------------------------------------------------------
+using namespace std;
+
+
+//==============================================================================
+// FUNCION PRINCIPAL - PUNTO DE INICIO DEL PROYECTO
+//------------------------------------------------------------------------------
+int main()
+{
+
+    //--------------------------------------------------------------------------
+    // FIN DE LA FUNCION main() SIN ERRORES.
+    //--------------------------------------------------------------------------
+    return 0;
+}
+
+//=============================================================================
+//                            FIN DE ARCHIVO
+//#############################################################################
+
