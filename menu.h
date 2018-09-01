3b9ce5c0db8b4d16957c6ed8dedec57b0332ca59
Pepito <juan.agustin.gonzalez99@gmail.com>
2018-09-01 12:07:51 -0300

    no hice nada

--- menu.h
+++ menu.h
@@ -1,40 +1,49 @@
 #ifndef MENU_H_INCLUDED
 #define MENU_H_INCLUDED
+
 #include <iostream>
 #include "archivos.h"
 
 using namespace std;
 
 
-void iNtro(){
+void intro()
+{
     cout<<"Hola"<<endl;
 }
 
-void sAlida(){
+void salida()
+{
     cout<<"Chau"<<endl;
 }
 
-void mEnu(){
+void menu()
+{
 
     bool salir = false;
-    char op[2];
+    while(!salir)
+    {
 
-    cout<<"Ingresate una opción del Menu: "<<endl;
-    sys::getline(op,2);
-    while(!salir){
-        switch(op[0]){
+        char op[2];
+        cout << "#=====================#" << endl;
+        cout << "|                     |" << endl;
+        cout << "|      1.             |" << endl;
+        cout << "|      2.             |" << endl;
+        cout << "|      3.             |" << endl;
+        cout << "|      4.             |" << endl;
+        cout << "|      5. Salir       |" << endl;
+        cout << "|                     |" << endl;
+        cout << "+---------------------+" << endl;
+
+        sys::getline(op,2);
+        switch(op[0])
+        {
 
 
 
-        }
-
-
-
-    }
-
-
-
-}
+        }//Fin switch
+    }//Fin while
+}//Fin menu()
 
 
 #endif // MENU_H_INCLUDED
